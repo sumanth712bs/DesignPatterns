b3b11822864a5bf1167044516e7f275479afc550
Badadal, Sumanth S <sbadadal@jcp.com>
2019-01-18 20:14:43 +0530

    Added project skeleton

---
+++ src/main/java/io/badadal/sumanth/designpatterns/DesignPatternsApplication.java
@@ -0,0 +1,14 @@
+package io.badadal.sumanth.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsApplication.class, args);
+	}
+
+}
+
